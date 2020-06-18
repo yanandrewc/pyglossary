d4fecc7a0e563fadab4b102bf6d89b6cd9135f6a
Saeed Rasooli <saeed.gnu@gmail.com>
2020-06-18 15:27:14 +0430

    WIP

--- pyglossary/plugins/wikipedia_dump.py
+++ pyglossary/plugins/wikipedia_dump.py
@@ -5,15 +5,22 @@ import re
 
 from formats_common import *
 
-enable = False
+enable = True
 format = "WikipediaDump"
-description = "Wikipedia Dump(Static HTML)"
-extensions = [".wiki"]
+description = "Wikipedia Dump (.xml)"
+extensions = [".xml"]
 optionsProp = {
 	"encoding": EncodingOption(),
+	"pageCount": IntOption(),
 }
-depends = {}
+depends = {
+#	"pijnu": "pijnu>=20160727",  # dependency of mediawiki-parser
+#	"mediawiki_parser": "mediawiki-parser",
+}
 
+
+
+"""
 BeautifulSoup = None
 
 def loadBeautifulSoup():
@@ -28,173 +35,123 @@ def loadBeautifulSoup():
 			f"{BeautifulSoup.__version__ !r} found.\n"
 			"Please run `sudo pip3 install lxml beautifulsoup4 html5lib`"
 		)
+"""
 
 class Reader(object):
-	specialPattern = re.compile(r".*~[^_]")
 	def __init__(self, glos):
 		self._glos = glos
-		self._rootDir = ""
-		self._articlesDir = ""
-		self._len = None
-		self._specialCount = 0
+		self._buff = b""
+		self._filename = ""
+		self._file = None
+		self._len = 0
 		# self._alts = {}
 		# { word => alts }
 		# where alts is str (one word), or list of strs
 		# we can't recognize alternates unless we keep all data in memory
 		# or scan the whole directiry and read all files twice
 
-	def open(self, dirname):
-		if not isdir(dirname):
-			raise IOError(f"{dirname} is not a directory")
-		self._rootDir = dirname
-		self._articlesDir = join(self._rootDir, "articles")
+	def _readUntil(self, sub: bytes) -> bytes:
+		for line in self._file:
+			if sub in line:
+				return line
+			self._buff += line
+
+	def _readSiteInfo(self) -> bytes:
+		self._buff = self._readUntil(b"<siteinfo>")
+		self._buff += self._readUntil(b"</siteinfo>")
+		siteinfoBytes = self._buff
+		self._buff = b""
+		return siteinfoBytes
+
+	def open(self, filename, pageCount=0):
+		try:
+			from lxml import etree
+		except ModuleNotFoundError as e:
+			e.msg += ", run `sudo pip3 install lxml` to install"
+			raise e
+		#try:
+		#	from mediawiki_parser.preprocessor import make_parser as make_preprocessor
+		#	from mediawiki_parser.html import make_parser
+		#except ModuleNotFoundError as e:
+		#	e.msg += ", run `sudo pip3 install mediawiki-parser` to install"
+		#	raise e
+
+		self._len = pageCount
+		self._filename = filename
+		self._file = open(filename, mode="rb")
+		
+		siteinfoBytes = self._readSiteInfo()
+		# TODO: parse siteinfoBytes
+
+		"""
+		templates = {}
+		allowed_tags = []
+		allowed_self_closing_tags = []
+		allowed_attributes = []
+		interwiki = {}
+		namespaces = {}
+
+		self._preprocessor = make_preprocessor(templates)
+		self._parser = make_parser(
+			allowed_tags,
+			allowed_self_closing_tags,
+			allowed_attributes,
+			interwiki,
+			namespaces,
+		)
+		"""
 
 	def close(self):
-		self._rootDir = ""
-		self._articlesDir = ""
-		self._len = None
+		self._filename = ""
+		self._file.close()
+		self._len = 0
 		# self._alts = {}
 
 	def __len__(self):
-		if not self._articlesDir:
-			log.error(
-				"WikipediaDump: called len(reader) while it's not open"
-			)
-			return 0
-		if self._len is None:
-			t0 = now()
-			log.info("Counting articles...")
-			self._len = sum(
-				len(files)
-				for _, _, files in os.walk(self._articlesDir)
-			)
-			log.debug(f"Counting articles took {now()-t0:.2f} seconds", )
-			log.info(f"Found {self._len} articles")
 		return self._len
 
-	def __iter__(self):
-		if not self._articlesDir:
+	def _readPage(self) -> "lxml.etree.Element":
+		from lxml import etree as ET
+		pageEnd = self._readUntil(b"</page>")
+		if pageEnd is None:
+			return
+		page = ET.fromstring(self._buff + pageEnd)
+		self._buff = b""
+		return page
+
+	def __iter__(self) -> "Iterator[BaseEntry]":
+		from lxml import etree as ET
+		if not self._filename:
 			log.error(
 				"WikipediaDump: trying to iterate over reader"
 				" while it's not open"
 			)
 			raise StopIteration
-		for dirpath, dirs, files in os.walk(self._articlesDir):
-			# dirpathRel = dirpath[len(self._articlesDir):].lstrip("/")
-			# dirParts = dirpathRel.split(os.sep)  # test on windows FIXME
-			# prefix = "".join([
-			#	chr(int(x, 16)) if len(x)==2 else x
-			#	for x in dirParts
-			# ])
-			dirs.sort()
-			files.sort()
-			for fname_html in files:
-				fpath = join(dirpath, fname_html)
-				fname, ext = splitext(fname_html)
-				# fpathRel = join(dirpathRel, fname_html)
-				if ext != ".html":
-					log.warning(f"unkown article extension: {fname_ext}")
-					continue
-				if self.isSpecialByPath(fname):  # , prefix
-					# log.debug(f"Skipping special page file: {fpathRel}")
-					self._specialCount += 1
-					yield None  # updates progressbar
-					continue
-				word = fname.replace("_", " ").replace("~", ":")
-				defi = self.parseArticle(word, fpath)
-				if not defi:
-					yield None  # updates progressbar
-					continue
+		while True:
+			page = self._readPage()
+			if page is None:
+				break
+			yield self._getEntryFromPage(page)
 
-				yield self._glos.newEntry(word, defi)
-		log.info(f"Skipped {self._specialCount} special page files")
 
-	def isSpecialByPath(self, fname):  # , d_prefix
-		"""
-			fname: str
-			d_prefix: str, with length of 3
-				this is the joint string version of directory relative path
-		"""
-		return re.match(self.specialPattern, fname)
-		# assert len(d_prefix) == 3
-		# f_prefix = fname[:3].lower()
-		# if f_prefix == d_prefix:
-		#	return False
-		# if len(f_prefix) < 3:
-		#	if f_prefix == d_prefix.rstrip("_"):
-		#		return False
-		# return True
-		# if "~" not in fname:
-		#	return False
-		# if fname[0] == dirParts[0]:
-		#	return False
-		# if list(fname[:3]) != dirParts:
-		#	log.debug(f"dirParts={dirParts!r}, fname={fname!r}")
-		# l_fname = fname.lower()
-		# for ext in ("png", "jpg", "jpeg", "gif", "svg", "pdf", "js"):
-		#	if "." + ext in l_fname:
-		#		return True
-		# return True
-
-	def parseArticle(self, word, fpath):
-		if BeautifulSoup is None:
-			loadBeautifulSoup()
-		try:
-			with open(fpath) as fileObj:
-				text = fileObj.read()
-		except UnicodeDecodeError:
-			log.error(f"error decoding file {fpath!r}, not UTF-8")
+	def _getEntryFromPage(self, page: "lxml.etree.Element") -> "BaseEntry":
+		# from wikimarkup.parser import parselite
+		from plugin_lib.wikimarkup.parser import parselite
+		titleElem = page.find(".//title")
+		if titleElem is None:
 			return
-		except:
-			log.exception(f"error reading file {fpath!r}")
+		title = titleElem.text
+		if not title:
 			return
-
-		root = BeautifulSoup(text, "lxml")
-		body = root.body
-		if not body:
+		textElem = page.find(".//text")
+		if textElem is None:
 			return
-
-#		if body.p and body.p.text.startswith("Redirecting to "):
-#			toWord = body.p.text[len("Redirecting to "):]
-#			try:
-#				fromWords = self._alts[toWord]
-#			except KeyError:
-#				self._alts[toWord] = word
-#			else:
-#				if isinstance(fromWords, str):
-#					self._alts[toWord] = [fromWords, word]
-#				else:
-#					fromWords.append(word)
-#			return
-#		try:
-#			alts = self._alts[word]
-
-		if body.p and body.p.text.startswith("Redirecting to "):
-			toWord = body.p.text[len("Redirecting to "):]
-			return f"↳ <a href=\"bword://{toWord}\">{toWord}</a>"
-			# "↳" does not look good for RTL languages FIXME
-
-		try:
-			content = body.find(id="column-content").find(id="content")
-		except AttributeError:
-			content = None
-		if not content:
-			log.warning(f"could not find \"content\" element: {fpath}")
+		text = textElem.text
+		if not text:
 			return
-
-		try:
-			firstHeading = content.find("h1", class_="firstHeading").text
-		except AttributeError:
-			log.warning(f"could not find \"firstHeading\" element: {fpath}")
-			return
-
-		if firstHeading != word:
-			log.debug(f"word={firstHeading!r}, firstHeading={word!r}")
-
-		bodyContent = content.find(id="bodyContent")
-		if not bodyContent:
-			log.warning(f"could not find \"bodyContent\" element: {fpath}")
-			return
-
-		# FIXME
-		return "".join([str(tag) for tag in bodyContent.contents])
+		# ptext = self._preprocessor.parse(text)
+		# log.info(f"type(ptext) = {type(ptext)}")
+		# arg to self._parser.parse must be a Node obj, not str
+		# text = str(self._parser.parse(ptext))
+		text = parselite(text)
+		return self._glos.newEntry(title, text)
